f2ddde6e6ce2a52fa5a565e468df9a888724edbf
Easy Coding tutorial <ecoding45@gmail.com>
2025-01-23 14:50:12 -0800

    first

--- app/Components/SliderBg/SliderBg.tsx
+++ app/Components/SliderBg/SliderBg.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState, useEffect } from 'react'
+
+import React from 'react'
 
 import styles from './SliderBg.module.css'  
 
@@ -11,33 +11,7 @@ const countdownDate = new Date('2025-05-19T23:18:07')
 
 export const SliderBg = () => {
 
-    const [currentImgIndex, setCurrentImgIndex] = useState(0)
 
-
-
-    // For Images Path
-    const ImgPath = [
-        '/assets/flower_Coming_Soon1.jpg',
-        '/assets/flower_Coming_Soon2.jpg', 
-    ]
-
-    // Function T o Go To The Next Image
-    const nextImg = () => {
-        setCurrentImgIndex((prevIndex) => (prevIndex + 1) % ImgPath.length)
-    }
-    // Function To Go To The Previous Image
-    const prevImg = () => {
-        setCurrentImgIndex((prevIndex) => (prevIndex - 1 + ImgPath.length) % ImgPath.length)
-    }
-
-    // Now Lets Change Bg Automatically
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            nextImg()
-        }, 5000) // Change Img In Every 5 Sec
-
-        return () => clearInterval(intervalId)
-    }, [])
 
   return (
     <div className={styles.SliderBg}>
@@ -49,7 +23,7 @@ export const SliderBg = () => {
                 </div>
             </div>
              <div>
-                 <img src={ImgPath[currentImgIndex]} alt="" />
+                 <video autoPlay muted loop src="/assets/swimmingpool_comingSoon.mp4"></video>
              </div>
              <div className={styles.Overlay}></div>
         </div>
@@ -65,14 +39,7 @@ export const SliderBg = () => {
 
           </div>
         </div>
-        <div className={styles.SliderControllers}>
-                <button
-                    onClick={prevImg}
-                    className={styles.PrevButton}>⬅</button>
-                <button
-                    onClick={nextImg}
-                    className={styles.NextButton}>➡</button>
-            </div>
+        
     </div>
   )
 }
